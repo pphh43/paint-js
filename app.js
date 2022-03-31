31e2fda7afdf2c1b3a4a0fc806fc420aa4a0df5e
Dorothy <dorothy@ntcon.co>
2022-03-31 17:57:14 +0900

    2.1 2D Context

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
- const canvas = document.getElementById("jsCanvas");
+ const canvas = document.getElementById("jsCanvas");//width, height 값 필수
+ canvas.width = 700;
+ canvas.height = 700;
+
+// canvas MDN document : https://developer.mozilla.org/ko/docs/Web/API/Canvas_API
+ const ctx = canvas.getContext("2d");
+ ctx.strokeStyle = "#2c2c2c";
+ ctx.lineWidth = 2.5;
 
  let painting = false;
 
@@ -7,14 +14,16 @@
     const x = event.offsetX;
     const y = event.offsetY;
     // console.log(x, y)
+    if(!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
  }
- function onMouseDown(event) {
+ function startPainting(event) {
     painting = true;
-    // console.log('painting')
- }
- function onMouseUp(event) {
-     stopPainting();
-
  }
  function stopPainting() {
      painting = false;
@@ -22,7 +31,7 @@
  }
  if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup",onMouseUp);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup",stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
  }
