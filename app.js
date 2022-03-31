10428ee587ac4893285c32f8ec739f46f4fc53d6
Dorothy <dorothy@ntcon.co>
2022-03-31 18:05:11 +0900

    2.2 Easy to manage canvas

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
  const canvas = document.getElementById("jsCanvas");//width, height 값 필수
- canvas.width = 700;
- canvas.height = 700;
+//  canvas.width = 700;
+canvas.width = document.getElementsByClassName("canvas")[0].offsetWidth;
+//  canvas.height = 700;
+canvas.height = document.getElementsByClassName("canvas")[0].offsetHeight;
 
 // canvas MDN document : https://developer.mozilla.org/ko/docs/Web/API/Canvas_API
  const ctx = canvas.getContext("2d");
@@ -15,11 +17,11 @@
     const y = event.offsetY;
     // console.log(x, y)
     if(!painting) {
-        ctx.beginPath();
-        ctx.moveTo(x, y);
+        ctx.beginPath();//경로 생성
+        ctx.moveTo(x, y);//선 시작 좌표
     } else {
-        ctx.lineTo(x, y);
-        ctx.stroke();
+        ctx.lineTo(x, y);//선 끝 좌표
+        ctx.stroke();//선 그리기
     }
  }
  function startPainting(event) {
